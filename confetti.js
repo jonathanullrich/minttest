422d38baca737eff0922b5e4480066af93925eee
Jonathan Ullrich <jonathan.ullrich@iese.fraunhofer.de>
2023-08-30 11:55:12 +0200

    changes

---
+++ confetti.js
@@ -0,0 +1,21 @@
+// confetti.js
+const confettiButton = document.getElementById("confettiButton");
+
+confettiButton.addEventListener("click", () => {
+    // Create a new confetti object
+    const confetti = new ConfettiGenerator({
+        target: "confettiContainer", // The ID of the container where confetti will be generated
+        max: 80, // Max number of confetti particles
+        colors: ["#ff0000", "#00ff00", "#0000ff", "#ff00ff", "#ffff00", "#00ffff"], // Array of confetti colors
+        size: 1, // Size of confetti particles
+        animate: true,
+    });
+
+    // Start the confetti animation
+    confetti.render();
+
+    // Remove confetti after 5 seconds (adjust the time as desired)
+    setTimeout(() => {
+        confetti.clear();
+    }, 5000);
+});
